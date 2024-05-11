34ca987fffdd63769c608e27135a76f0aa629f53
enjanurr <bajentingjanurr@gmail.com>
2024-05-11 11:16:34 +0800

    fizz

---
+++ fizz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(n):
+    for i in range(1,n+1):
+        giif i % 5 == 0 and i % 3 == 0:
+             print('fizzbuzz')
+           
+        elif i % 5 == 0:
+             print('buzz')
+        elif i % 3 == 0: 
+             print('fizzbuzz')
+        print(i)
+fizzbuzz(100)
+
+        
+
