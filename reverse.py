0b608b361d596dd3c922816250b9624094a3c070
enjanurr <bajentingjanurr@gmail.com>
2024-05-13 20:32:52 +0800

    reverse

---
+++ reverse.py
@@ -0,0 +1,9 @@
+def reverse_string(inputString):
+    reverse_string = inputString[::-1]
+    return reverse_string
+
+inputString = "hello love goodbye"
+input_result = reverse_string(inputString)
+print('original: ', inputString)
+print('reversed: ', input_result)
+
