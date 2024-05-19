a6d0ed18d5b37820967eb7f969a7605605a186d2
enjanurr <bajentingjanurr@gmail.com>
2024-05-19 20:13:51 +0800

    pyra

---
+++ return.py
@@ -0,0 +1,5 @@
+def count_arguments(*args):
+    return len(args)
+
+print(count_arguments(1, 2, 3))  # Output: 3
+print(count_arguments('hello', 'world'))  # Output: 2
---
+++ todolist.py
@@ -0,0 +1,20 @@
+def add_task(): 
+    task = str(input('add task: '))
+    add_task = task
+  
+def view_task():
+
+
+
+def main():
+    tasks = []
+
+    while True:
+        print("\n===== To-Do List =====")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Mark Task as Done")
+        print("4. Exit")
+
+
+    
