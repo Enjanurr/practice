2b720e8b59785fb30c7580796b998fa95c7e1915
enjanurr <bajentingjanurr@gmail.com>
2024-05-24 20:39:24 +0800

    try

---
+++ cal.py
@@ -0,0 +1,9 @@
+
+def calculate_area(length , width):
+    area = length * width 
+    print('your answer is: ', area)
+    return area
+
+lenght = float(input('Lenght: '))
+width = float(input('widht: '))
+calculate_area(lenght , width)
